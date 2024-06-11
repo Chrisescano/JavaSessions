fce048443134aa9012fe41671cd993f53272758f
Christian Escano <escanochristian21@gmail.com>
2024-06-10 23:32:54 -0400

    Graded session1 to session3

--- PracticeProblems/src/main/java/javasession2/Main.java
+++ PracticeProblems/src/main/java/javasession2/Main.java
@@ -1,38 +1,70 @@
 package javasession2;
 
+import java.util.Scanner;
+
 public class Main {
-    System.out.println("Getting user input makes your programs dynamic and fun.");
-    //Problem 2
-    System.out.println("Java.Util.Scanner");
-    //Problem 3
-    System.out.println("Java libary")
-    //Problem 4
-    System.out.println("nextDouble()");
-    //Problem 5
-    System.out.println("What part is declaring Scanner?" + "Scanner input" /n "What part is initializing the Scanner?" +
-            "new Scanner(SystemIn)")
-    //Problem 6
-      String name;
-      int age;
-    System.out.println("Hello " + name + "I see you are " + age + "years old")
-    //Problem 7
-    int[] names = new int[10];
-    //Problem 8
-    String[] names = new String[4]
-            names[0]= Kerry;
-            names[1]= Christian;
-            names[2]= Nichole;
-            names[3]= Sahara;
-    System.out.println("Hello my name is " + names[1] + "and this is " + names[3] + "it is nice to meet you" names[0] + "and" + names[2]);
-    //Problem 9
-    System.out.println("What happens is an error");
-    //Problem 10
-    String name;
-    System.out.println("Enter the name of a character: " + names[1] + /n "Hey " + names[1] +
-            " welcome to the country of something, in this" +
-            "country we have nothing going on, no food no water, just rocks and dirt. " +
-            "We sell dirt cookie and mud water $15.00 each. We also have" +
-            "mud slide and a place for you too catch a nice disease. " +
-            "All in all welcome to SOMETHING!" );
+
+    public static Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        //result 6.5/10
+        //problem 1
+        System.out.println("Getting user input makes your programs dynamic and fun."); //[correct]
+
+        //Problem 2
+        System.out.println("java.util.Scanner"); //[partial]
+
+        //Problem 3
+        System.out.println("Java libary"); //[correct]
+
+        //Problem 4
+        System.out.println("nextDouble()"); //[correct]
+
+        //Problem 5
+        System.out.println("What part is declaring Scanner?" + "Scanner input\nWhat part is initializing the Scanner?" +
+                "new Scanner(System.in)"); //[partial]
+
+        //Problem 6
+        String name = scanner.nextLine();
+        int age = scanner.nextInt();
+        scanner.nextLine(); //clears buffer
+        System.out.println("Hello " + name + "I see you are " + age + "years old"); //[partial]
+
+        //Problem 7
+        int[] randomarray = new int[10]; //[correct]
+
+        //Problem 8
+        String[] names = new String[4];
+        names[0]= "Kerry";
+        names[1]= "Christian";
+        names[2]= "Nichole";
+        names[3]= "Sahara";
+        System.out.println("Hello my name is " + names[0] + "and this is " + names[2] + "it is nice to meet you" + names[3] + "and" + names[1]); //[wrong]
+
+        //Problem 9
+        System.out.print("Please type in an integer (string): ");
+        int number = scanner.nextInt();
+        System.out.println("What happens is an error"); //[partial]
+
+        //Problem 10
+        String characterName = scanner.nextLine();
+        System.out.println("Enter the name of a character: " + characterName + "\n Hey " + characterName +
+                " welcome to the country of something, in this" +
+                "country we have nothing going on, no food no water, just rocks and dirt. " +
+                "We sell dirt cookie and mud water $15.00 each. We also have" +
+                "mud slide and a place for you too catch a nice disease. " +
+                "All in all welcome to SOMETHING!" ); //[partial]
+
+        //extra credit
+        System.out.print("Enter your full name: ");
+        String fullName = scanner.nextLine();
+        String[] nameParts = fullName.split(" ");
+
+        System.out.println("First name: "+nameParts[0]);
+        System.out.println("Middle name: "+nameParts[1]);
+        System.out.println("Last name: "+nameParts[2]);
+    }
+
 
 }
--- PracticeProblems/src/main/java/javasession3/Main.java
+++ PracticeProblems/src/main/java/javasession3/Main.java
@@ -1,34 +1,56 @@
 package javasession3;
 
 public class Main {
-    //Problem 1
-    System.out.println("Class file");
-    //Problem 2
-    System.out.println("To group related classes, interfaces and sub packages.");
-    //Problem 3
-    System.out.println("Methods are great way of PACKAGING code together, that PERFORMS a task.");
-    //Problem 4
-    Sysytem.out.println("Its the beginning of a Java project");
-    //Problem 5
-    System.out.println("True, False, True, True, True, False");
-    //Problem 6
-    System.out.println("Public, Protect, default, Private");
-    //Problem 7
-    int threeNumbers(int a, int b, int c){
-        return a*b+c;
-    }
 
-    //Problem 8
-    String dumbStory(String james, String apple){
-        System.out.println("There once was a boy named " james  " His favorite fruits are " apple ". " +
-            "One day " james " was hungry" +
-            " so he went search for an " apple " he hunted and hunted but found no " apple
-            " but then " james "realized " +
-            "he was looking in the orange section.");
+    public String globalVar = "Hello World";
+
+    public static void main(String[] args) {
+
+        //total 7.5/9
+
+        //Problem 1
+        System.out.println("Class file"); //[correct]
+
+        //Problem 2
+        System.out.println("To group related classes, interfaces and sub packages."); //[correct]
+
+        //Problem 3
+        System.out.println("Methods are great way of PACKAGING code together, that PERFORMS a task."); //[correct]
+
+        //Problem 4
+        System.out.println("Its the beginning of a Java project"); //[correct]
+
+        //Problem 5
+        System.out.println("True, False, True, True, True, False"); // 3/6 [partial]
+
+        //Problem 6
+        System.out.println("Public, Protected, default, Private"); //[correct]
+
+        //Problem 7
+//        int threeNumbers(int a, int b, int c){
+//            return a*b+c;
+//        } //[correct]
+
+        //Problem 8
+//        void dumbStory(String james, String apple) {
+//            System.out.println("There once was a boy named " + james + " His favorite fruits are " + apple + ". " +
+//                            "One day " + james + " was hungry" +
+//                            " so he went search for an " + apple + " he hunted and hunted but found no " + apple +
+//                    " but then " + james + "realized " +
+//                            "he was looking in the orange section.");
+//        } //[partial]
+
+        //Problem 9
+//        void printFirstCharacter(String name) {
+//            char firstChar = name.charAt(0);
+//            System.out.println("Original String: " + name + " change made : " + firstChar);
+//        } //[partial]
+        // When you want to call out an object you have to type out . then the method, in this case the string method.
+
+        //extra credit
+//        void changeGlobalVar(String change) {
+//            globalVar = change;
+//        }
+        //writing setters
     }
-    //Problem 9
-    String something(String name){
-        name.charAt(int index);
-    }
-    // When you want to call out an object you have to type out . then the method, in this case the string method.
 }
