08cbf4d9db07f1d7a43396ba621f3fc907a0a5df
Christian Escano <escanochristian21@gmail.com>
2024-06-11 22:05:41 -0400

    Added Madlib project template

---
+++ MadlibProject/src/main/java/Main.java
@@ -0,0 +1,46 @@
+public class Main {
+
+    /**
+     * This where your program comes together. Follow the steps from the project
+     * directions and try to piece together everything.
+     */
+    public static void main(String[] args) {
+
+    }
+
+    /**
+     * This method should present to the user all the story that they can choose from.
+     * for example:
+     *
+     * We have the following stories:
+     * (1) - story1
+     * (2) - story2
+     * ...
+     *
+     * please type in story number: [user types in here]
+     *
+     * This method should return the story name that the user choose
+     */
+    public static String presentStoryOptionsAndReturnStoryName() {
+        return null;
+    }
+
+    /**
+     * This method should print to the terminal a prompt to let the user know
+     * what they should do.
+     * Then the method should use the scanner class to get a string input.
+     * Finally, it should return the string input
+     */
+    public static String getUserString(String prompt) {
+        return null;
+    }
+
+    /**
+     * This method should run the logic for the first story.
+     * It should store all the user inputs into variables.
+     * At the end it should print the full story with the words filled in
+     */
+    public static void runStory1() {
+
+    }
+}
