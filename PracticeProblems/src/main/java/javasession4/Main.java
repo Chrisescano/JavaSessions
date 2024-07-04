73a6fbd703802bc0188c3d26a15da913c1f6c85d
Christian Escano <escanochristian21@gmail.com>
2024-07-03 22:30:05 -0400

    Added conditional statement exercise

---
+++ PracticeProblems/src/main/java/javasession4/Main.java
@@ -0,0 +1,71 @@
+package javasession4;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        /*
+        what is a conditional operation?
+        1. are you wearing red shoes? yes or no
+        2. is 5 greater than 6? yes or no
+         */
+
+        /*
+        what do the conditional operations look like?
+        >, <, >=, <=, !, !=, ==
+
+        note: this will only work for primitives
+         */
+
+        /*
+        we will use conditional operations inside if statements
+         */
+
+//        if (a > b) { //start of codeblock
+//            //code
+//        } //end of codeblock
+
+//        if (a < b) {
+//            //code
+//        } else {
+//            //code
+//        }
+
+//        if (a < b) {
+//
+//        } else if (a == b) {
+//
+//        }
+
+        //ternary operator
+        //value = conditional statement ? choice1 : choice2;
+//        String name = "Kerry";
+//        int value = name.equals("Christian") ? 1 : 2;
+//        System.out.println(value);
+
+        //switch statements
+//        Scanner scanner = new Scanner(System.in);
+//        int month = scanner.nextInt();
+//
+//        switch (month) {
+//            case 1:
+//                System.out.println("January");
+//                break; //always end you cases with the break keyword
+//            case 2:
+//                System.out.println("February");
+//                break;
+//            default:
+//                System.out.println("That is not a month");
+//        }
+
+        //conditional operators - >, <, >=, <=, !, !=, ==
+        //if statements (just the if the statements - can only 1 decision
+        //else statements - these will run when the previous if OR else if statement is false
+        //else if statements - go between if and else and you can have multiple
+        //ternary operator - a one line of code to simplify if followed by else - that can also return a value
+        //switch statements - allow you to compare a value, against multiple cases, and even gives you a default case
+            //if none of the cases match
+    }
+
+}
